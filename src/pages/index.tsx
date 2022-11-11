f914e4a8455e2e57753ccf652729c445b872f50c
heesoo <11junghs@gmail.com>
2022-11-11 20:47:03 +0900

    chore : 로고 렌더링 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,51 @@
 import styled from 'styled-components'
 import { useRouter } from 'next/router'
-import Head from 'next/head'
-import Image from 'next/image'
-import Logo from '../../public/img/netflix_5.gif'
+import { useEffect, useRef } from 'react'
+import lottie from "lottie-web";
 import styles from '../../styles/Home.module.css'
-import { useEffect } from 'react'
 
 export default function Home() {
   const router = useRouter();
 
-  useEffect(() => {
-    
+  const netflixContainer = useRef<HTMLDivElement>(null);
+
+  useEffect(()=>{
+    lottie.loadAnimation({
+      container: netflixContainer.current as HTMLDivElement,
+      renderer: 'svg',
+      loop: false,
+      autoplay:true,
+      animationData:require("../../public/img/netflix-logo.json")
+     
+    })
+
     const timer = setTimeout(() => {
       router.push('/home');
-    },3100);
+    },3000);
 
     return () => {
       clearTimeout(timer);
     };
     
-  },[]);
+
+  },[])
 
   return (
-    <div className={styles.container}>
-
-      <Image 
-        src={Logo}
-        alt="Logo of Netflix"
-        width={500}
-      />
-      
-    </div>
+    <Container>
+        <Item ref={netflixContainer}></Item>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Item =styled.div`
+  width: 375px;
+  height: 100%;
+  position: fixed;
+  z-index: 1000;
+`
